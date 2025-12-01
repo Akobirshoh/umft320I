fcea023efc490c14cadf7cee8da0fa53052bd7e8
AKOSH <akobirinoyatov0@gmail.com>
2025-12-01 17:28:47 +0500

    patch

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from fastapi import FastAPI
 from funcInoyatov import inoyatov
 from functionilyas import ilyas
-from funkost import kostin
+from funckost import kostin
+from pydantic import BaseModel
+
+
+class TwoNumbers(BaseModel):
+    x: float
+    y: float
 
 print(inoyatov(25,5))
 print(kostin(3,7))
@@ -8,9 +15,26 @@ print(kostin(3,7))
 print(inoyatov(25,5))
 print(ilyas(3,4))
 
+app = FastAPI( title="UMFT320I", version="1.0.0",
+description="Платформа для покупки и продажи",
+docs_url="/docs",
+redoc_url="/redoc",
+#debug=settings.DEBUG, 
+)
+
+
 @app.get("/c2")
 def get_ilyas(x: float, y: float):
     return {"result": ilyas(x, y)}
 @app.post("/c2")
 def post_ilyas(data: TwoNumbers):
     return {"result": ilyas(data.x, data.y)}
+
+@app.get("/inoyatov")
+def get_inoyatov(x: float, y: float ):
+    return {"result": inoyatov(x, y)}
+@app.post("/inoyatov")
+def post_inoyatov(data: TwoNumbers):
+    return {"result": inoyatov(data.x, data.y)}
+
+
